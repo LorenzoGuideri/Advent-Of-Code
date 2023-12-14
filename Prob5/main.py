54592643887993e9bfe2af8557228ab44c86cfac
Lorenzo Guideri <lorenzo.guideri@proton.me>
2023-12-14 18:35:49 +0100

    Fixing loginx's bug.

--- Prob5/main.py
+++ Prob5/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 def part1():
     with open("./input.txt") as f:
         data=list(filter(None,f.read().split('\n')))
@@ -24,28 +25,36 @@ def part1():
 def part2():
     with open("./input.txt") as f:
         data=list(filter(None,f.read().split('\n')))
-    s=[int(i) for i in data[0].split() if i!="seeds:"]
-    seeds=[]
-    for i in range(0,len(s),2):
-        seeds+=[i for i in range(s[i],s[i]+s[i+1])]
-        
-    
+    seeds=[int(i) for i in data[0].split() if i!="seeds:"]
+    seeds=[(seeds[i],seeds[i]+seeds[i+1]) for i in range(0,len(seeds),2)]
     data=data[1:]+["foo bar"]
     M=[]
     for row in data:
         numbers=row.split()
         if len(numbers)==2:
             if M:
-                for i in range(len(seeds)):
-                    for j in M:
-                        if seeds[i]>=int(j[1]) and seeds[i]<int(j[1])+int(j[2]):
-                            seeds[i]=seeds[i]-int(j[1])+int(j[0])
+                new = []
+                while len(seeds) > 0:
+                    s, e = seeds.pop()
+                    for a, b, c in M:
+                        os = max(s, b)
+                        oe = min(e, b + c)
+                        if os < oe:
+                            new.append((os - b + a, oe - b + a))
+                            if os > s:
+                                seeds.append((s, os))
+                            if e > oe:
+                                seeds.append((oe, e))
                             break
+                    else:
+                        new.append((s, e))
+                seeds = new
             M=[]
         else:
-            M+=[numbers]
+            M+=[list(map(int,numbers))]
     print(min(seeds))
     return
+
 
 if __name__ == "__main__":
 
